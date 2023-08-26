2d8c92927938b9f14c0a54ea5dfa591eca1568ca
bhushan354 <127464886+bhushan354@users.noreply.github.com>
2023-08-26 08:52:02 +0530

    Update allFunctions.js according to project requirements

--- src/module/allFunctions.js
+++ src/module/allFunctions.js
@@ -1,31 +1,38 @@
-import tasks from './taskData.js';
+const tasks = require('./taskData.js');
 
-// localsstrorage saving function
-export function saveInLocalstorage() {
+function saveInLocalstorage() {
   localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
-export function changeIndexes(tasks) {
+function changeIndexes(tasks) {
   tasks.forEach((task, index) => {
     task.index = index + 1;
   });
 }
 
-export function addNewTask(description) {
+function addNewTask(description) {
   const newTask = { description, completed: false, index: tasks.length + 1 };
   tasks.push(newTask);
   changeIndexes(tasks);
   saveInLocalstorage();
 }
 
-export function removeTask(index) {
+function removeTask(index) {
   tasks.splice(index - 1, 1);
   changeIndexes(tasks);
   saveInLocalstorage();
 }
 
-export function editTask(index, newDescription, newCompleted) {
+function editTask(index, newDescription, newCompleted) {
   tasks[index - 1].description = newDescription;
   tasks[index - 1].completed = newCompleted;
   saveInLocalstorage();
 }
+
+module.exports = {
+  addNewTask,
+  removeTask,
+  editTask,
+  changeIndexes,
+  saveInLocalstorage,
+};
